12e12b68d06606dfa798f47326293ddbaafaf4f2
DINESH THIYAGARAJAN <dineshthiyagu1097@gmail.com>
2025-08-20 11:10:25 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -55,7 +55,7 @@ wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/sha
 echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
 sudo apt update && sudo apt install terraform -y
 
-echo "=== Install AWS CLI ==="
+echo "=== Install AWS CLI ===" 
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 sudo apt-get install unzip -y
 unzip awscliv2.zip
